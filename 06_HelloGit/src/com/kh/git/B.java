ca9110669b3a18d1aafd0824c782f120ab0980b7
jinha <jinhak94@naver.com>
2021-03-30 12:28:14 +0900

    주석 추가

--- 06_HelloGit/src/com/kh/git/B.java
+++ 06_HelloGit/src/com/kh/git/B.java
@@ -3,6 +3,9 @@ package com.kh.git;
 public class B {
 	public int poo() {
 		// 원격저장소에서 작성함.
+		// fetch는 원격의 내용을 단지 바라보고 있는 것으로,
+		// 읽기 전용으로 쓰기 불가능하다.
+		// merge를 해서 가져와야 가능. pull이 이것까지 한 번에 해주는 것.
 		System.out.println("안녕하세요.");
 		return 0;
 	}
