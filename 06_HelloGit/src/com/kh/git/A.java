1c628e6f61ba48608953ef7b8d5c5b7037826856
jinha <jinhak94@naver.com>
2021-03-30 10:49:05 +0900

    A

--- 06_HelloGit/src/com/kh/git/A.java
+++ 06_HelloGit/src/com/kh/git/A.java
@@ -1,5 +1,13 @@
 package com.kh.git;
-
+/*
+ * revert
+ *  이후 변경내역 남김
+ * 
+ * reset
+ *  hard HEAD, Index and WorkingTree : 지정한 버젼으로 돌아가고,이후 변경내역을 모두 삭제함.
+	soft HEAD Only: 지정한 버젼으로 돌아가고,이후 변경내역은 index(staging area)에 남음
+	mixed HEAD And Index: 기본값. 지정한 버젼으로 돌아가고, index도 초기화됨. uncommited changes 에 남음.
+ */
 public class A {
 	public void foo() {
 		
